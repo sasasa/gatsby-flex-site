7c71cc59a7e346f3cc29b6cbbd9c28195abe4f43
sasasa <masaakisaeki@gmail.com>
2020-11-07 17:49:59 +0900

    hero image css change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Image from "../components/image";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { faChartBar, faChevronRight, faCogs, faRocket } from "@fortawesome/free-solid-svg-icons";
-
+import Fruits from "../../content/assets/fruits.jpg";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -16,7 +16,11 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle} className="">
       <SEO title="All posts" />
 
-<section className="conA">
+<section className="conA"
+	style={{
+		backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(${Fruits})`
+	}}
+>
 <div className="container">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>{".a{fill:none;stroke:#fff;stroke-width:6.59px;}"}</style></defs><title>logo-color</title><rect class="a" x="18.44" y="18.28" width="64.05" height="64.05" transform="translate(-21.09 49.72) rotate(-45)"/><line class="a" x1="48.86" y1="6.58" x2="48.86" y2="70.62"/><line class="a" x1="28.5" y1="26.64" x2="28.5" y2="70.06"/><line class="a" x1="29.87" y1="49.6" x2="92.14" y2="49.6"/><line class="a" x1="25.2" y1="70.62" x2="73.92" y2="70.62"/></svg>
 	<h1>LOGGER</h1>
